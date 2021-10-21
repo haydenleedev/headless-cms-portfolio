2c95d5bb2730d583da0356a6380d1f6acf95117e
Max I <max@ikius.com>
2021-10-21 14:20:42 +0300

    marketo optimize CLS

--- components/agility-pageModules/resourceContent/resourceContent.jsx
+++ components/agility-pageModules/resourceContent/resourceContent.jsx
@@ -2,7 +2,7 @@ import { renderHTML } from "@agility/nextjs";
 import { AgilityImage } from "@agility/nextjs";
 import Head from "next/head";
 import Script from "next/script";
-
+import style from "./resourceContent.module.scss"
 const ResourceContent = ({ dynamicPageItem }) => {
   const resource = dynamicPageItem.fields;
   return (
@@ -15,14 +15,16 @@ const ResourceContent = ({ dynamicPageItem }) => {
           window.MktoForms2.loadForm("//info.ujet.co", "205-VHT-559", 1638);
         }}
       />
+      {/* <Script src="//info.ujet.co/js/forms2/js/forms2.min.js" />
+      <Script id="marketo-js">{`window.MktoForms2.loadForm("//info.ujet.co", "205-VHT-559", 1638);`}</Script> */}
       <section className="section">
         <div className="container">
           <div className="columns repeat-2">
-            <div>
+            <div className={style.content}>
               <h1 className="heading-5">{resource.title}</h1>
               <div className="content mt" dangerouslySetInnerHTML={renderHTML(resource.text)} />
             </div>
-            <div>
+            <div className={style.form}>
               <form id="mktoForm_1638"></form>
             </div>
           </div>
