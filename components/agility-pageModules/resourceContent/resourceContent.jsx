dc10906435f0521317813999e9d54bbf06c8bd63
Max I <max@ikius.com>
2021-10-21 13:50:09 +0300

    marketo form load optimization

--- components/agility-pageModules/resourceContent/resourceContent.jsx
+++ components/agility-pageModules/resourceContent/resourceContent.jsx
@@ -1,15 +1,19 @@
 import { renderHTML } from "@agility/nextjs";
 import { AgilityImage } from "@agility/nextjs";
 import Head from "next/head";
+import Script from "next/script";
 
 const ResourceContent = ({ dynamicPageItem }) => {
   const resource = dynamicPageItem.fields;
-  console.log(resource);
   return (
     <>
-      <Head>
-        <script async src="//info.ujet.co/js/forms2/js/forms2.min.js"></script>
-      </Head>
+      <Script
+        id="marketo-js"
+        src="//info.ujet.co/js/forms2/js/forms2.min.js"
+        onLoad={() => {
+          window.MktoForms2.loadForm("//info.ujet.co", "205-VHT-559", 1638);
+        }}
+      />
       <section className="section">
         <div className="container">
           <div className="columns repeat-2">
@@ -18,10 +22,6 @@ const ResourceContent = ({ dynamicPageItem }) => {
               <div className="content mt" dangerouslySetInnerHTML={renderHTML(resource.text)} />
             </div>
             <div>
-              <script
-                async
-                dangerouslySetInnerHTML={{ __html: `MktoForms2.loadForm("//info.ujet.co", "205-VHT-559", 1638);` }}
-              ></script>
               <form id="mktoForm_1638"></form>
             </div>
           </div>
