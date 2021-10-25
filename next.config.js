1ff90d73c3c80938821230feb3c73d5a6c9a887a
Max I <max@ikium.net>
2021-10-25 13:34:44 +0300

    next config changes

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/test',
-        destination: '/blog', // Matched parameters can be used in the destination
-        permanent: true,
-      },
-      {
         source: '/wp-content/:path*/:slug',
         destination: 'https://assets.ujet.cx/:slug', // Matched parameters can be used in the destination
         permanent: true,
