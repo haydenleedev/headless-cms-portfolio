780b83232802fd249d95614fbc94a34e1187c00a
Aatu Väisänen <aatu@ikius.com>
2021-10-27 14:58:17 +0300

    add key attribute to mapped cards

--- components/agility-pageModules/blogPostList/blogPostList.jsx
+++ components/agility-pageModules/blogPostList/blogPostList.jsx
@@ -13,7 +13,12 @@ const BlogPostList = ({ module, overrideClass, blogPosts }) => {
 
         <div className={style.cards}>
           {blogPosts.map((blogPost) => (
-            <GenericCard title={blogPost.fields.title} link={blogPost.url} image={blogPost.fields.image}></GenericCard>
+            <GenericCard
+              key={blogPost.contentID}
+              title={blogPost.fields.title}
+              link={blogPost.url}
+              image={blogPost.fields.image}
+            ></GenericCard>
           ))}
         </div>
       </div>
