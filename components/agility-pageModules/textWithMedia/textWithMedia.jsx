a9dcb1accdd9785e3a92f907dcc9063d3e87d291
Aatu Väisänen <aatu@ikius.com>
2021-10-27 17:35:53 +0300

    validate agility true/false input values

--- components/agility-pageModules/textWithMedia/textWithMedia.jsx
+++ components/agility-pageModules/textWithMedia/textWithMedia.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { boolean } from "../../../utils/validation";
 import Heading from "../heading";
 import Media from "../media";
 import style from "./textWithMedia.module.scss";
@@ -11,24 +12,24 @@ const TextWithMedia = ({ module }) => {
       <div className="container">
         <div
           className={`${style.content} ${
-            fields.columnLayout
+            boolean(fields.columnLayout)
               ? "flex-direction-column justify-content-center align-items-center"
-              : fields.mediaLeft
+              : boolean(fields.mediaLeft)
               ? "flex-direction-row-reverse"
               : "flex-direction-row"
           }`}
         >
           <div
             className={`${style.textContent} ${
-              fields.columnLayout
+              boolean(fields.columnLayout)
                 ? "justify-content-center align-items-center"
-                : fields.mediaLeft
+                : boolean(fields.mediaLeft)
                 ? "justify-content-flex-end align-items-flex-start"
                 : "justify-content-flex-start align-items-flex-start"
             }`}
           >
             {heading.text && (
-              <div className={fields.columnLayout ? "heading" : style.heading}>
+              <div className={boolean(fields.columnLayout) ? "heading" : style.heading}>
                 <Heading {...heading} />
               </div>
             )}
@@ -40,7 +41,7 @@ const TextWithMedia = ({ module }) => {
               <Link href={fields.link.href}>
                 <a
                   className={`mt button ${
-                    !fields.columnLayout ? "small" : ""
+                    !boolean(fields.columnLayout) ? "small" : ""
                   } cyan outlined ${style.link}`}
                   aria-label={`Navigate to page ` + fields.link.href}
                   title={`Navigate to page ` + fields.link.href}
@@ -50,7 +51,7 @@ const TextWithMedia = ({ module }) => {
               </Link>
             )}
           </div>
-          <div className={`${style.media} ${fields.mediaLeft ? "mr" : "ml"}`}>
+          <div className={`${style.media} ${boolean(fields.mediaLeft) ? "mr" : "ml"}`}>
             {fields.media && <Media media={fields.media} />}
           </div>
         </div>
---
+++ utils/validation.js
@@ -0,0 +1,13 @@
+// utility functions for checking input validity
+
+// use for checking true/false values coming from Agility.
+export const boolean = (input) => {
+  switch (input?.toLowerCase()) {
+    case "false":
+      return false;
+    case "true":
+      return true;
+    default:
+      return null;
+  }
+};
