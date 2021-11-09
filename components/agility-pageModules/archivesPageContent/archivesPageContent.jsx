5cbbc72929fee8528e71070772a598a8fe04b199
Aatu Väisänen <aatu@ikius.com>
2021-11-09 11:13:26 +0200

    changes to archivesPageContent

--- components/agility-pageModules/archivesPageContent/archivesPageContent.jsx
+++ components/agility-pageModules/archivesPageContent/archivesPageContent.jsx
@@ -200,28 +200,151 @@ const ArchivesPageContent = ({ customData }) => {
           )) || <Loader />}
         </div>
         <footer className={style.pagination}>
-          <button
-            className={`reset-button ${style.previousPageButton}`}
-            onClick={previousPage}
-            disabled={activePageNumber === 0}
-          ></button>
-          {totalPagesCount &&
-            [...Array(totalPagesCount).keys()].map((pageNumber) => (
-              <div className={pageNumber === activePageNumber ? "w-600" : ""}>
-                <button
-                  className={`reset-button ${style.pageButton}`}
-                  key={pageNumber}
-                >
-                  {pageNumber + 1}
-                </button>
-              </div>
-            ))}
-
-          <button
-            className={`reset-button ${style.nextPageButton}`}
-            onClick={nextPage}
-            disabled={activePageNumber + 1 === totalPagesCount}
-          ></button>
+          <div className="d-flex">
+            <button
+              className={`reset-button ${style.previousPageButton}`}
+              onClick={previousPage}
+              disabled={activePageNumber === 0}
+            ></button>
+            {totalPagesCount && (
+              <>
+                {totalPagesCount < 8 ? (
+                  [...Array(totalPagesCount).keys()].map((pageNumber) => (
+                    <div
+                      className={pageNumber === activePageNumber ? "w-600" : ""}
+                    >
+                      <button
+                        className={`reset-button ${style.pageButton}`}
+                        onClick={() => {
+                          setCurrentOffset(pageNumber * PER_PAGE);
+                          setActivePageNumber(pageNumber);
+                        }}
+                        key={pageNumber}
+                      >
+                        {pageNumber + 1}
+                      </button>
+                    </div>
+                  ))
+                ) : (
+                  <>
+                    <div className={activePageNumber === 0 ? "w-600" : ""}>
+                      <button
+                        className={`reset-button ${style.pageButton}`}
+                        onClick={() => {
+                          setCurrentOffset(0);
+                          setActivePageNumber(0);
+                        }}
+                        key={1}
+                      >
+                        1
+                      </button>
+                    </div>
+                    {activePageNumber < 4 && (
+                      <>
+                        {[...Array(totalPagesCount).keys()]
+                          .slice(1, 4)
+                          .map((pageNumber) => (
+                            <div
+                              className={
+                                pageNumber === activePageNumber ? "w-600" : ""
+                              }
+                            >
+                              <button
+                                className={`reset-button ${style.pageButton}`}
+                                onClick={() => {
+                                  setCurrentOffset(pageNumber * PER_PAGE);
+                                  setActivePageNumber(pageNumber);
+                                }}
+                                key={pageNumber}
+                              >
+                                {pageNumber + 1}
+                              </button>
+                            </div>
+                          ))}
+                        ...
+                      </>
+                    )}
+                    {activePageNumber > 3 &&
+                      activePageNumber < totalPagesCount - 3 && (
+                        <>
+                          ...
+                          {[...Array(totalPagesCount).keys()]
+                            .slice(activePageNumber - 1, activePageNumber + 2)
+                            .map((pageNumber) => (
+                              <div
+                                className={
+                                  pageNumber === activePageNumber ? "w-600" : ""
+                                }
+                              >
+                                <button
+                                  className={`reset-button ${style.pageButton}`}
+                                  onClick={() => {
+                                    setCurrentOffset(pageNumber * PER_PAGE);
+                                    setActivePageNumber(pageNumber);
+                                  }}
+                                  key={pageNumber}
+                                >
+                                  {pageNumber + 1}
+                                </button>
+                              </div>
+                            ))}
+                          ...
+                        </>
+                      )}
+                    {activePageNumber > totalPagesCount - 4 && (
+                      <>
+                        ...
+                        {[...Array(totalPagesCount).keys()]
+                          .slice(totalPagesCount - 4, totalPagesCount - 1)
+                          .map((pageNumber) => (
+                            <div
+                              className={
+                                pageNumber === activePageNumber ? "w-600" : ""
+                              }
+                            >
+                              <button
+                                className={`reset-button ${style.pageButton}`}
+                                onClick={() => {
+                                  setCurrentOffset(pageNumber * PER_PAGE);
+                                  setActivePageNumber(pageNumber);
+                                }}
+                                key={pageNumber}
+                              >
+                                {pageNumber + 1}
+                              </button>
+                            </div>
+                          ))}
+                      </>
+                    )}
+                    <div
+                      className={
+                        activePageNumber === totalPagesCount - 1 ? "w-600" : ""
+                      }
+                    >
+                      <button
+                        className={`reset-button ${style.pageButton}`}
+                        onClick={() => {
+                          setCurrentOffset(totalPagesCount - 1 * PER_PAGE);
+                          setActivePageNumber(totalPagesCount - 1);
+                        }}
+                        key={totalPagesCount - 1}
+                      >
+                        {totalPagesCount}
+                      </button>
+                    </div>
+                  </>
+                )}
+              </>
+            )}
+            <button
+              className={`reset-button ${style.nextPageButton}`}
+              onClick={nextPage}
+              disabled={activePageNumber + 1 === totalPagesCount}
+            ></button>
+          </div>
+          <p>
+            Showing {activePageNumber + 1} of {totalPagesCount}
+          </p>
         </footer>
       </nav>
     </section>
