3e199b65c97c0a6f7e7caad89d04ab91677d46f1
Aatu Väisänen <aatu@ikius.com>
2021-11-17 16:12:26 +0200

    add blogPageContent

---
+++ components/agility-pageModules/blogPageContent/blogCard.jsx
@@ -0,0 +1,42 @@
+import { AgilityImage } from "@agility/nextjs";
+import Link from "next/link";
+import { toDate } from "../../../utils/convert";
+import { hrefSelf } from "../../../utils/validation";
+import style from "./blogCard.module.scss";
+
+const BlogCard = ({ title, image, link, date, category }) => {
+  const isInnerLink = hrefSelf(link.href);
+  return (
+    <Link href={isInnerLink ? link : link.href}>
+      <a
+        aria-label={"Navigate to blog post: " + title}
+        title={title}
+        target={link?.target}
+      >
+        <div className={style.blogCard}>
+          {image && image.pixelWidth && (
+            <div className={style.image}>
+              <AgilityImage
+                src={image.url}
+                alt={image.label || null}
+                width="250"
+                height="162"
+                objectFit="cover"
+              />
+            </div>
+          )}
+          <div className={style.textContent}>
+            {date && <p className={style.date}>{toDate(date)}</p>}
+            {category && <p className={style.category}>{category}</p>}
+            <p className={style.title}>{title}</p>
+            <p className={style.link}>
+              <span>Read more</span>
+            </p>
+          </div>
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+export default BlogCard;
