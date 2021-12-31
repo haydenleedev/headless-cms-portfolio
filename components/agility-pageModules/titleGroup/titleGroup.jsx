1941f89b74bac9c441b006f82f24da5c4838239e
haydenleedev <haydenlee22@gmail.com>
2021-12-31 15:31:18 -0500

    fixed boolean import

--- components/agility-pageModules/titleGroup/titleGroup.jsx
+++ components/agility-pageModules/titleGroup/titleGroup.jsx
@@ -1,5 +1,6 @@
 import Heading from "../heading";
 import { useIntersectionObserver } from "../../../utils/hooks";
+import { boolean } from "../../../utils/validation";
 import style from "./titleGroup.module.scss";
 
 const TitleGroup = ({ module }) => {
