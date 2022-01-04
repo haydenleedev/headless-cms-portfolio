7902284f345c0541c1359a3de53455d466aebd27
haydenleedev <haydenlee22@gmail.com>
2022-01-04 13:42:59 -0500

    updated space module

--- components/agility-pageModules/spacer/spacer.jsx
+++ components/agility-pageModules/spacer/spacer.jsx
@@ -1,11 +1,13 @@
-import style from "./spacer.module.scss"
+import style from "./spacer.module.scss";
 
 const Spacer = ({ module }) => {
   const { fields } = module;
   return (
-    <section
-      className={`section ${style.spacer} ${fields.classes ? fields.classes : ""}`}
-    ></section>
+    <div
+      className={`section ${style.spacer} ${
+        fields.classes ? fields.classes : ""
+      }${fields.spaceHeight ? " " + fields.spaceHeight : " pt-3 pb-3"}`}
+    ></div>
   );
 };
 
