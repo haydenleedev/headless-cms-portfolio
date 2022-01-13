98fc96b7a7d99a8295897d26a3f78e3b9a6c09f6
Aatu Väisänen <aatu@ikius.com>
2022-01-13 15:06:27 +0200

    add touch controls to slider

--- components/slider/slide.jsx
+++ components/slider/slide.jsx
@@ -1,16 +1,20 @@
 import { forwardRef } from "react";
 import style from "./slide.module.scss";
 
-const Slide = forwardRef(({ maxWidth, children }, ref) => {
-  return (
-    <div
-      className={style.slide}
-      style={{ maxWidth: `${maxWidth}px` }}
-      ref={ref}
-    >
-      {children}
-    </div>
-  );
-});
+const Slide = forwardRef(
+  ({ maxWidth, handleTouchStart, handleTouchMove, children }, ref) => {
+    return (
+      <div
+        className={style.slide}
+        style={{ maxWidth: `${maxWidth}px` }}
+        ref={ref}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+      >
+        {children}
+      </div>
+    );
+  }
+);
 Slide.displayName = "Slide";
 export default Slide;
