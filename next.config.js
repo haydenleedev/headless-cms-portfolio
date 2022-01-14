e1f520b80f4142a2b4f2ecf498b63e7c77bb7289
Max I <max@ikius.com>
2022-01-14 18:03:03 +0200

    add blog rewrite

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 module.exports = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // if the host is `app.acme.com`,
+        // this rewrite will be applied
+        {
+          source: "/:path*",
+          has: [
+            {
+              type: "host",
+              value: "blog.ujet.co",
+            },
+          ],
+          destination: "/blog/:path*",
+        },
+      ],
+    };
+  },
   async redirects() {
     return [
       {
