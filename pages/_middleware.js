ef61eea3efc90d689127836a3fc5653f8e0f2ed5
Max I <max@ikius.com>
2022-01-17 19:44:47 +0200

    middleware logic for uppercase redirects

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,11 +1,14 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
+
+  // // Handle URL's that contain uppercase characters
+  if (req.nextUrl.pathname !== req.nextUrl.pathname.toLowerCase()) 
+    return NextResponse.redirect(`${req.nextUrl.origin}${req.nextUrl.pathname.toLowerCase()}`);
+
   let url = req.url;
   url = url.replace(/en-US/g, "");
-  
-  // // Handle URL's that contain uppercase characters
-  if (url.toLowerCase() !== url) return NextResponse.redirect(url.toLowerCase());
+  // if (url.toLowerCase() !== url) return NextResponse.redirect(url.toLowerCase());
   
   // Handle blog.ujet.co redirects to ujet.cx/blog/postname
   const blogUrl = "blog.ujet.co";
