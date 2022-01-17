fa481996db6b4484632efdaa4467f78d5f25f37b
Max I <max@ikius.com>
2022-01-17 18:28:11 +0200

    add middleware rules to handle urls with upper case characters

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-  const url = req.url;
+  const url = req.url.replace(/en-US/g, "");
   
   // Handle URL's that contain uppercase characters
   if (url.toLowerCase() !== url) return NextResponse.redirect(url.toLowerCase());
@@ -10,7 +10,7 @@ export async function middleware(req) {
   const blogUrl = "blog.ujet.co";
   const blogUrlRegex = new RegExp(`/(${blogUrl})/`);
   if (url.includes(blogUrl)) {
-    const postSlug = url.replace(/en-US/g, "").split(blogUrlRegex)[2];
+    const postSlug = url.split(blogUrlRegex)[2];
     const redirectUrl = "https://ujet.cx/blog";
     if (postSlug) {
       return NextResponse.redirect(`${redirectUrl}/${postSlug}`);
