b76aed400eea3f3ee12a986cc6497c6a330aa7bb
Max I <max@ikius.com>
2022-01-17 17:53:03 +0200

    remove locale from middleware url redirect script

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,8 +4,9 @@ export async function middleware(req) {
   const url = req.url;
   const blogUrl = "blog.ujet.co";
   const blogUrlRegex = new RegExp(`/(${blogUrl})/`);
+  console.log(url.split(blogUrlRegex)[2]);
   if (url.includes(blogUrl)) {
-    const postSlug = url.split(blogUrlRegex)[2];
+    const postSlug = url.replace(/en-US/g, "").split(blogUrlRegex)[2];
     const redirectUrl = "https://ujet.cx/blog";
     if (postSlug) {
       return NextResponse.redirect(`${redirectUrl}/${postSlug}`);
