931c593e2896403d093ec23e4677b42d25ba7192
Max I <max@ikius.com>
2022-01-18 10:31:35 +0200

    middleware changes

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,19 +2,16 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
   const url = req.url;
-  
-  // Handle blog.ujet.co redirects to ujet.cx/blog/postname
   const blogUrl = "blog.ujet.co";
   const blogUrlRegex = new RegExp(`/(${blogUrl})/`);
+  console.log(url.split(blogUrlRegex)[2]);
   if (url.includes(blogUrl)) {
-    const postSlug = url.split(blogUrlRegex)[2];
+    const postSlug = url.replace(/en-US/g, "").split(blogUrlRegex)[2];
     const redirectUrl = "https://ujet.cx/blog";
     if (postSlug) {
       return NextResponse.redirect(`${redirectUrl}/${postSlug}`);
     }
     return NextResponse.redirect(redirectUrl);
   }
-
-  // All other cases do nothing.
   return NextResponse.next();
 }
