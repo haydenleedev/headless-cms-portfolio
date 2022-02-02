8df6d5f290891bfdee71631f9963f17fc2b7f242
Max I <max@ikius.com>
2022-02-02 16:53:37 +0200

    conditionally render Jquery if toggled

--- components/agility-pageModules/scriptLoader/scriptLoader.jsx
+++ components/agility-pageModules/scriptLoader/scriptLoader.jsx
@@ -1,7 +1,15 @@
+import Head from "next/head";
+
 const ScriptLoader = ({ module }) => {
     const { fields } = module;
+    
     return (
         <section className="section">
+            <Head>
+                {fields.jquery && (
+                    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+                )}
+            </Head>
             <div className="container">
                 <div dangerouslySetInnerHTML={{ __html: fields.html }} />
             </div>
