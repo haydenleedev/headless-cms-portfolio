24854a0bf7108563bc3e7a4a77e8a4e07ff0a822
Max I <max@ikius.com>
2022-02-02 17:01:00 +0200

    Script loader changes

--- components/agility-pageModules/scriptLoader/scriptLoader.jsx
+++ components/agility-pageModules/scriptLoader/scriptLoader.jsx
@@ -1,20 +1,26 @@
 import Head from "next/head";
+import Script from "next/script";
 
 const ScriptLoader = ({ module }) => {
-    const { fields } = module;
-    
-    return (
-        <section className="section">
-            <Head>
-                {fields.jquery && (
-                    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-                )}
-            </Head>
-            <div className="container">
-                <div dangerouslySetInnerHTML={{ __html: fields.html }} />
-            </div>
-        </section>
-    );
-}
+  const { fields } = module;
+
+  return (
+    <section className="section">
+      <Head>
+        {fields.jquery && (
+          <Script
+            id="jqeury"
+            src="https://code.jquery.com/jquery-1.12.4.min.js"
+            integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+            crossorigin="anonymous"
+          ></Script>
+        )}
+      </Head>
+      <div className="container">
+        <div dangerouslySetInnerHTML={{ __html: fields.html }} />
+      </div>
+    </section>
+  );
+};
 
 export default ScriptLoader;
