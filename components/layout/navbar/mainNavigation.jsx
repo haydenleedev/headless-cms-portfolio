fa8f31b4444d9f113864daca2b1c67a3395dde6e
Kim B <kim@ikius.com>
2022-03-02 17:18:34 +0200

    Add check to navigation item sort

--- components/layout/navbar/mainNavigation.jsx
+++ components/layout/navbar/mainNavigation.jsx
@@ -25,7 +25,7 @@ const MainNavigation = ({
   mainNavigation.forEach((group) => {
     if (group.fields.columns?.length > 0) {
       group.fields.columns.forEach((column) => {
-        column.fields.items.sort(function (a, b) {
+        column.fields.items?.sort(function (a, b) {
           return b.properties.itemOrder + a.properties.itemOrder;
         });
       });
