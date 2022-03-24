f164bd9938ead7cb97373864847abe21fbbd991e
Kim B <kim@ikius.com>
2022-03-24 17:10:25 +0200

    Accordion refactor (anims temporarily disabled)

--- components/agility-pageModules/accordion/accordion.jsx
+++ components/agility-pageModules/accordion/accordion.jsx
@@ -1,42 +1,45 @@
 import { renderHTML } from "@agility/nextjs";
-import { useState } from "react/cjs/react.development";
+import { useRef } from "react";
 import { sanitizeHtmlConfig } from "../../../utils/convert";
 import style from "./accordion.module.scss";
 
 const Accordion = ({ customData }) => {
-  const items = customData.items;
-  items.sort(function (a, b) {
+  const { itemsWithSanitizedHTML } = customData;
+  const detailsRefs = useRef([]);
+
+  const closeOtherItems = (excludedIndex) => {
+    detailsRefs.current.forEach((item, index) => {
+      if (index !== excludedIndex) {
+        item.open = false;
+      }
+    });
+  };
+
+  itemsWithSanitizedHTML?.sort(function (a, b) {
     return a.properties.itemOrder - b.properties.itemOrder;
   });
-  const [expandedItem, setExpandedItem] = useState(null);
+
   return (
     <section className="section">
       <div className="container">
         <div className={style.accordion}>
-          {items.map((item, index) => {
+          {itemsWithSanitizedHTML.map((item, index) => {
             return (
-              <div
-                className={`${style.accordionItem} ${
-                  expandedItem == index && style.expanded
-                }`}
-                key={`accordionItem${index}`}
-                aria-expanded={expandedItem == index}
-                onFocus={() => setExpandedItem(index)}
+              <details
+                className={style.accordionItem}
+                onClick={() => closeOtherItems(index)}
+                ref={(elem) => (detailsRefs.current[index] = elem)}
               >
-                <div
-                  className={style.itemToggle}
-                  tabIndex={0}
-                  aria-label={`Open accordion item ${item.fields.heading}`}
-                >
+                <summary className={style.itemToggle}>
                   <div className={style.chevron} />
                   <h3 className="heading-5 text-darkblue">
                     {item.fields.heading}
                   </h3>
-                </div>
+                </summary>
                 <div className={style.itemContentWrapper}>
                   <div dangerouslySetInnerHTML={renderHTML(item.fields.html)} />
                 </div>
-              </div>
+              </details>
             );
           })}
         </div>
@@ -46,14 +49,14 @@ const Accordion = ({ customData }) => {
 };
 
 Accordion.getCustomInitialProps = async function ({ item }) {
-  const { items } = item.fields;
   const sanitizeHtml = (await import("sanitize-html")).default;
   // sanitize unsafe HTML ( all HTML entered by users and any HTML copied from WordPress to Agility)
   const cleanHtml = (html) => sanitizeHtml(html, sanitizeHtmlConfig);
-  items.forEach((item) => {
-    item.fields.html = item.fields.html ? cleanHtml(item.fields.html) : null;
+  const itemsWithSanitizedHTML = item.fields.items.map((item) => {
+    item.fields.html = cleanHtml(item.fields.html);
+    return item;
   });
-  return { items };
+  return { itemsWithSanitizedHTML };
 };
 
 export default Accordion;
