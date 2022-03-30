4acb6b4edee183b6db047499c2fa93989e087b8c
Kim B <kim@ikius.com>
2022-03-30 16:58:58 +0300

    Add HorizontallyScrollableList component

--- components/agility-pageModules/secondaryNav/secondaryNav.jsx
+++ components/agility-pageModules/secondaryNav/secondaryNav.jsx
@@ -3,13 +3,24 @@ import style from "./secondaryNav.module.scss";
 import Heading from "../heading";
 import { useRef, useEffect, useContext } from "react";
 import GlobalContext from "../../../context";
+import HorizontallyScrollableList from "../../horizontallyScrollableList/horizontallyScrollableList";
 
 const SecondaryNav = ({ module }) => {
   const { fields } = module;
   const heading = fields.heading ? JSON.parse(fields.heading) : null;
   const sectionRef = useRef();
   const { navbarRef } = useContext(GlobalContext);
-  const linkContainerRef = useRef();
+
+  const links = fields.links.map((link, index) => {
+    return (
+      <AgilityLink
+        agilityLink={{ href: link.fields.link.href }}
+        key={`link${index}`}
+      >
+        <p>{link.fields.link.text}</p>
+      </AgilityLink>
+    );
+  })
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -31,65 +42,16 @@ const SecondaryNav = ({ module }) => {
     };
   }, []);
 
-  const scrollHorizontally = (left) => {
-    const element = linkContainerRef.current;
-    const maxScrollValue = element.scrollWidth - element.clientWidth;
-    const currentScrollPercentage = (
-      element.scrollLeft / maxScrollValue
-    ).toFixed(2);
-    const scrollPoints = [0, 0.25, 0.5, 0.75, 1];
-    let targetScrollPercentage;
-    if (currentScrollPercentage == 1 && !left) {
-      targetScrollPercentage = 0;
-    } else if (currentScrollPercentage == 0 && left) {
-      targetScrollPercentage = 1;
-    } else {
-      if (left) {
-        for (let i = scrollPoints.length - 1; i > 0; i--) {
-          if (currentScrollPercentage > scrollPoints[i]) {
-            targetScrollPercentage = scrollPoints[i];
-            break;
-          }
-        }
-      } else {
-        for (let i = 0; i < scrollPoints.length; i++)
-          if (currentScrollPercentage < scrollPoints[i]) {
-            targetScrollPercentage = scrollPoints[i];
-            break;
-          }
-      }
-    }
-    element.scrollLeft = maxScrollValue * targetScrollPercentage;
-  };
-
   return (
     <>
       {fields.links && (
         <section className={`section ${style.section}`} ref={sectionRef}>
           <nav className={`container ${style.nav}`}>
             {heading && <Heading {...heading} />}
-            <div className={style.linksWrapper}>
-              <button
-                className={`${style.scrollButton} reset-button`}
-                onClick={() => scrollHorizontally(true)}
-              />
-              <div className={`${style.links}`} ref={linkContainerRef}>
-                {fields.links.map((link, index) => {
-                  return (
-                    <AgilityLink
-                      agilityLink={{ href: link.fields.link.href }}
-                      key={`link${index}`}
-                    >
-                      <p>{link.fields.link.text}</p>
-                    </AgilityLink>
-                  );
-                })}
-              </div>
-              <button
-                className={`${style.scrollButton} reset-button`}
-                onClick={() => scrollHorizontally(false)}
-              />
-            </div>
+            <HorizontallyScrollableList
+              items={links}
+              maxVisibleItems={4}
+            />
           </nav>
         </section>
       )}
---
+++ components/horizontallyScrollableList/horizontallyScrollableList.jsx
@@ -0,0 +1,71 @@
+import { useRef } from "react";
+import style from "./horizontallyScrollableList.module.scss";
+
+const HorizontallyScrollableList = ({ items, maxVisibleItems = 4 }) => {
+  const itemContainerRef = useRef();
+  const scrollHorizontally = (left) => {
+    const element = itemContainerRef.current;
+    const elementWidth = element.clientWidth;
+    const maxScrollValue = element.scrollWidth - element.clientWidth;
+    const currentScrollValue = parseInt(element.scrollLeft);
+    const scrollPoints = [];
+    for (let i = 0; i < items.length; i++) {
+      scrollPoints.push(
+        (parseInt((elementWidth / maxVisibleItems) * i))
+        )
+    }
+    let targetScrollValue;
+    if (currentScrollValue == maxScrollValue && !left) {
+      targetScrollValue = 0;
+    } else if (currentScrollValue == 0 && left) {
+      targetScrollValue = maxScrollValue;
+    } else {
+      if (left) {
+        for (let i = scrollPoints.length - 1; i > 0; i--) {
+          if (currentScrollValue > scrollPoints[i]) {
+            targetScrollValue = scrollPoints[i];
+            break;
+          }
+        }
+      } else {
+        for (let i = 0; i < scrollPoints.length; i++)
+          if (currentScrollValue < scrollPoints[i]) {
+            targetScrollValue = scrollPoints[i];
+            break;
+          }
+      }
+    }
+    element.scrollLeft = targetScrollValue;
+  };
+
+  return (
+    <>
+      {items && (
+        <div className={style.itemsWrapper}>
+          <button
+            className={`${style.scrollButton} reset-button`}
+            onClick={() => scrollHorizontally(true)}
+          />
+          <div className={`${style.items}`} ref={itemContainerRef}>
+            {items.map((item, index) => {
+              return (
+                <div
+                  key={`item${index}`}
+                  style={{flexBasis: `calc(calc(100% / (${maxVisibleItems})`}}
+                >
+                  {item}
+                </div>
+              )
+            })}
+          </div>
+          <button
+            className={`${style.scrollButton} reset-button`}
+            onClick={() => scrollHorizontally(false)}
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default HorizontallyScrollableList;
