5a745d6b2751be6e616a3b13355d53e26168c396
haydenleedev <haydenlee22@gmail.com>
2022-04-06 13:43:44 -0400

    added heading to the heroImage module

--- components/agility-pageModules/heroImage/heroImage.jsx
+++ components/agility-pageModules/heroImage/heroImage.jsx
@@ -1,9 +1,11 @@
 import { boolean } from "../../../utils/validation";
 import Media from "../media";
+import Heading from "../heading";
 import style from "./heroImage.module.scss";
 
 const HeroImage = ({ module, narrowHeight }) => {
   const { fields } = module;
+  const heading = fields.heading ? JSON.parse(fields.heading) : null;
   const containerWidth = boolean(fields.containerWidth);
   const narrowContainer = boolean(fields?.narrowContainer);
 
@@ -16,6 +18,11 @@ const HeroImage = ({ module, narrowHeight }) => {
       }`}
       id={fields.id ? fields.id : null}
     >
+      {heading && (
+        <div className={`container ${style.overlayText}`}>
+          <Heading {...heading} />
+        </div>
+      )}
       {fields.image && <Media media={fields.image} />}
     </section>
   );
