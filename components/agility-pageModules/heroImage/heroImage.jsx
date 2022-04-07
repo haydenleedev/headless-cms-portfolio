7ae0fe7d8730261bf71e7856c1c483efc8f8f3a9
haydenleedev <haydenlee22@gmail.com>
2022-04-07 09:58:00 -0400

    updated heroImage module

--- components/agility-pageModules/heroImage/heroImage.jsx
+++ components/agility-pageModules/heroImage/heroImage.jsx
@@ -1,10 +1,13 @@
 import { boolean } from "../../../utils/validation";
 import Media from "../media";
+import { renderHTML } from "@agility/nextjs";
+import { sanitizeHtmlConfig } from "../../../utils/convert";
 import Heading from "../heading";
 import style from "./heroImage.module.scss";
 
-const HeroImage = ({ module, narrowHeight }) => {
+const HeroImage = ({ module, narrowHeight, customData }) => {
   const { fields } = module;
+  const { sanitizedHtml } = customData;
   const heading = fields.heading ? JSON.parse(fields.heading) : null;
   const containerWidth = boolean(fields.containerWidth);
   const narrowContainer = boolean(fields?.narrowContainer);
@@ -21,11 +24,30 @@ const HeroImage = ({ module, narrowHeight }) => {
       {heading && (
         <div className={`container ${style.overlayText}`}>
           <Heading {...heading} />
+          <div
+            className={`container content`}
+            dangerouslySetInnerHTML={renderHTML(sanitizedHtml)}
+          ></div>
         </div>
       )}
       {fields.image && <Media media={fields.image} />}
     </section>
   );
+};
+
+HeroImage.getCustomInitialProps = async function ({ item }) {
+  const sanitizeHtml = (await import("sanitize-html")).default;
+  // sanitize unsafe HTML ( all HTML entered by users and any HTML copied from WordPress to Agility)
+
+  const cleanHtml = (html) => sanitizeHtml(html, sanitizeHtmlConfig);
+
+  const sanitizedHtml = item.fields.description
+    ? cleanHtml(item.fields.description)
+    : null;
+
+  return {
+    sanitizedHtml,
+  };
 };
 
 export default HeroImage;
