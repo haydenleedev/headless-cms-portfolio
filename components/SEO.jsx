14050091a674ff5ee54137b1083da0ec65ab701f
Kim B <kim@ikius.com>
2022-04-11 15:04:53 +0300

    Google Tag Manager script performance test 2

--- components/SEO.jsx
+++ components/SEO.jsx
@@ -77,18 +77,18 @@ const SEO = ({ title, description, keywords, metaHTML, url }) => {
         {`function OptanonWrapper() { }`}
       </Script> */}
 
-      {/* <Script id="google-tag-manager">
+      <Script id="google-tag-manager">
         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');`}
-        </Script> */}
-      <Script
+        </Script>
+      {/* <Script
         id="google-optimize"
         src={`${googleOptimize}${process.env.NEXT_PUBLIC_GOOGLE_OPTIMIZE_ID}`}
         strategy="lazyOnload"
-      />
+      /> */}
 
       {/* <Script id="bombora"> */}
         {/* Bombora Tag */}
