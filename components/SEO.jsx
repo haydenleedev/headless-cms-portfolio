342f3f5b1fffd47157cb31ae6e942342014b2d5e
Kim B <kim@ikius.com>
2022-04-11 15:33:12 +0300

    Revert script test commits

--- components/SEO.jsx
+++ components/SEO.jsx
@@ -83,20 +83,20 @@ const SEO = ({ title, description, keywords, metaHTML, url }) => {
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');`}
-        </Script>
-      {/* <Script
+      </Script>
+      <Script
         id="google-optimize"
         src={`${googleOptimize}${process.env.NEXT_PUBLIC_GOOGLE_OPTIMIZE_ID}`}
         strategy="lazyOnload"
-      /> */}
+      />
 
-      {/* <Script id="bombora"> */}
+      <Script id="bombora">
         {/* Bombora Tag */}
-        {/*{`
-          informer
+        {`
+          //informer
           (function(f,i,c){var a=decodeURIComponent,e="",l="",o="||",g=";;",h="split",b="length",j="indexOf",k=0,n="localStorage",m="_ccmdt";f[c]=f[c]||{};function d(q){var p;if(f[n]){return f[n][q]||""}else{p=i.cookie.match(q+"=([^;]*)");return(p&&p[1])||""}}f[c].us={};e=a(d(m))[h](o);k=e[b];if(k>0){while(k--){l=e[k][h]("=");if(l[b]>1){if(l[1][j](g)>-1){f[c].us[l[0]]=l[1][h](g);f[c].us[l[0]].pop()}else{f[c].us[l[0]]=l[1]}}}}})(window,document,"_ml");
 
-          tag
+          //tag
           (function () {
             _ml = window._ml || {};
             _ml.eid = '84421';
@@ -128,9 +128,9 @@ const SEO = ({ title, description, keywords, metaHTML, url }) => {
             s.parentNode.insertBefore(mltag, s);
           })();
         `}
-      </Script> */}
+      </Script>
 
-      {/* <Script id="6sense">
+      <Script id="6sense">
         {`window._6si = window._6si || [];
           window._6si.push(['enableEventTracking', true]);
           window._6si.push(['setToken', '${process.env.NEXT_PUBLIC_SIXSENSE_TOKEN}']);
@@ -144,10 +144,10 @@ const SEO = ({ title, description, keywords, metaHTML, url }) => {
             var s = document.getElementsByTagName('script')[0];
             s.parentNode.insertBefore(gd, s);
           })();`}
-      </Script> */}
+      </Script>
 
       {/* Qualified Script */}
-      {/* <Script id="qualified">
+      <Script id="qualified">
         {`(function(w,q){w['QualifiedObject']=q;w[q]=w[q]||function(){
           (w[q].q=w[q].q||[]).push(arguments)};})(window,'qualified')`}
       </Script>
@@ -156,7 +156,7 @@ const SEO = ({ title, description, keywords, metaHTML, url }) => {
         async
         src={`${qualifiedSrc}${process.env.NEXT_PUBLIC_QUALIFIED_TOKEN}`}
         strategy="lazyOnload"
-      /> */}
+      />
     </>
   );
 };
