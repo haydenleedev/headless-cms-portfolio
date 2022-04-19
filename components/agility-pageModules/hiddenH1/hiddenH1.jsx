c3fc3c72035835c26aa42fd7f767cbe94b2d3f33
Kim B <kim@ikius.com>
2022-04-19 14:37:04 +0300

    Fix Navbar scroll state issue caused by HiddenH1

--- components/agility-pageModules/hiddenH1/hiddenH1.jsx
+++ components/agility-pageModules/hiddenH1/hiddenH1.jsx
@@ -3,7 +3,7 @@ import style from "./hiddenH1.module.scss";
 const HiddenH1 = ({ module }) => {
     const { fields } = module;
     return (
-        <h1 id={style.heading}>{fields.text}</h1>
+        <h1 id={style.heading} data-hidden-h1="true">{fields.text}</h1>
     );
 }
 
