4c42a0eb13580ac39b240e164825f6671426f01f
Kim B <kim@ikius.com>
2022-04-19 14:40:27 +0300

    Change HiddenH1 id to classname

--- components/agility-pageModules/hiddenH1/hiddenH1.jsx
+++ components/agility-pageModules/hiddenH1/hiddenH1.jsx
@@ -1,10 +1,12 @@
 import style from "./hiddenH1.module.scss";
 
 const HiddenH1 = ({ module }) => {
-    const { fields } = module;
-    return (
-        <h1 id={style.heading} data-hidden-h1="true">{fields.text}</h1>
-    );
-}
+  const { fields } = module;
+  return (
+    <h1 className={style.heading} data-hidden-h1="true">
+      {fields.text}
+    </h1>
+  );
+};
 
 export default HiddenH1;
