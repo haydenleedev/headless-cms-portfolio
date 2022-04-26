6011ac309800b880e0ce860ff2fd8cd01e56ff96
Kim B <kim@ikius.com>
2022-04-26 12:46:06 +0300

    Make BlankCards use the GenericCard component

--- components/agility-pageModules/blankCards/blankCards.jsx
+++ components/agility-pageModules/blankCards/blankCards.jsx
@@ -1,99 +1,46 @@
-import { AgilityImage, renderHTML } from "@agility/nextjs";
-import { sanitizeHtmlConfig } from "../../../utils/convert";
 import { boolean } from "../../../utils/validation";
-import AgilityLink from "../../agilityLink";
+import GenericCard from "../../genericCard/genericCard";
 import style from "./blankCards.module.scss";
 
-const BlankCards = ({ module, customData }) => {
-    const { fields } = module;
-    const cards = customData;
-    cards.sort(function (a, b) {
-        return a.properties.itemOrder - b.properties.itemOrder;
-    });
-    const RenderCard = ({ card }) => {
-        const isIconCard = boolean(card.fields.useImageAsIcon);
-        return (
-            <div className={style.cardWrapper}>
-                <div className={style.card}>
-                    {(card.fields.image && !isIconCard) && (
-                        <div className={`${style.imageWrapper} ${cards.length < fields.maxCardsPerRow ? style[`height${cards.length}`] : style.height4}`}>
-                            <AgilityImage
-                                src={card.fields.image.url}
-                                width={0}
-                                height={0}
-                                layout="responsive"
-                                objectFit="cover"
-                            />
-                        </div>
-                    )}
-                    <div className={`${style.textContent} ${(card.fields.image && !isIconCard) && style.imageCardTextContent}`}>
-                        {(card.fields.title && isIconCard) && (
-                            <div className={`${style.titleWithIcon} ${cards.length < fields.maxCardsPerRow ? style[`height${cards.length}`] : style.height4}`}>
-                                {card.fields.image && (
-                                    <div className={style.iconWrapper}>
-                                        <AgilityImage
-                                            src={card.fields.image.url}
-                                            width={0}
-                                            height={0}
-                                            layout="responsive"
-                                            objectFit="contain"
-                                            className={style.icon}
-                                        />
-                                    </div>
-                                )}
-                                <p className={style.title}>{card.fields.title}</p>
-                            </div>
-                        )}
-                        {(card.fields.title && !isIconCard) && (
-                            <p className={style.title}>{card.fields.title}</p>
-                        )}
-                        {card.fields.text && (
-                            <div
-                                dangerouslySetInnerHTML={renderHTML(card.fields.text)}
-                                className={fields.textAlignment == "left" ? "align-left" : "align-center"}
-                            />
-                        )}
-                        {card.fields.link && (
-                            <p className={style.linkText}><span >{card.fields.link.text}</span></p>
-                        )}
-                    </div>
-                </div>
-            </div>
-        );
-    }
-    return (
-        <section className="section">
-            <div className="container">
-                <div className={style.cardGrid}>
-                    {cards.map((card, index) => {
-                        return (
-                            <div
-                                key={`card${index}`}
-                                className={cards.length < fields.maxCardsPerRow ? style[`flexBasis${cards.length}`] : style[`flexBasis${fields.maxCardsPerRow}`]}
-                            >
-                                {card.fields.link ?
-                                    <AgilityLink agilityLink={card.fields.link} className={style.linkCard}>
-                                        <RenderCard card={card} />
-                                    </AgilityLink>
-                                    : <RenderCard card={card} />
-                                }
-                            </div>
-                        );
-                    })}
-                </div>
-            </div>
-        </section>
-    );
-}
-
-BlankCards.getCustomInitialProps = async function({ item }) {
-    const items = item.fields.cards;
-    const sanitizeHtml = (await import("sanitize-html")).default;
-    // sanitize unsafe HTML ( all HTML entered by users and any HTML copied from WordPress to Agility)
-    const cleanHtml = (html) => sanitizeHtml(html, sanitizeHtmlConfig);
-    items.forEach((item) => {
-        item.fields.text = item.fields.text ? cleanHtml(item.fields.text) : null;
-    });
-    return items;
+const BlankCards = ({ module }) => {
+  const { fields } = module;
+  const cards = fields.cards;
+  cards.sort(function (a, b) {
+    return a.properties.itemOrder - b.properties.itemOrder;
+  });
+  return (
+    <section className="section">
+      <div className="container">
+        <div className={style.cardGrid}>
+          {cards.map((card, index) => {
+            return (
+              <div
+                className={`
+                  ${
+                    cards.length < fields.maxCardsPerRow
+                      ? style[`flexBasis${cards.length}`]
+                      : style[`flexBasis${fields.maxCardsPerRow}`]
+                  }`}
+              >
+                <GenericCard
+                  key={`card${index}`}
+                  link={card.fields.link}
+                  title={card.fields.title}
+                  image={card.fields.image}
+                  ariaTitle={card.fields.title}
+                  description={card.fields.description}
+                  configuration={{
+                    iconStyleImage: boolean(card.fields.useImageAsIcon),
+                    descriptionAlignment: fields.textAlignment,
+                  }}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
+
 export default BlankCards;
---
+++ components/genericCard/renderGenericCard.jsx
@@ -0,0 +1,140 @@
+import { AgilityImage } from "@agility/nextjs";
+import { toDate, resolveCategory } from "../../utils/convert";
+
+const RenderGenericCard = ({ properties }) => {
+  const {
+    image,
+    configuration,
+    title,
+    category,
+    date,
+    newsSite,
+    podcast,
+    description,
+    link,
+    overrideCategory,
+  } = properties;
+  return (
+    <div className="genericCard">
+      {(image || configuration?.defaultImage) && (
+        <>
+          {configuration?.iconStyleImage ? (
+            <div
+              className={`genericCard__titleWithIcon ${
+                configuration?.imageHeight
+                  ? `genericCard__titleWithIcon--${configuration?.imageHeight}`
+                  : ""
+              }`}
+            >
+              <div className={`genericCard__titleWithIcon--icon `}>
+                {image && image.pixelWidth ? (
+                  <AgilityImage
+                    src={image.url}
+                    alt={image.label || ""}
+                    width={image.pixelWidth}
+                    height={image.pixelHeight}
+                    objectFit="cover"
+                    layout="responsive"
+                  />
+                ) : (
+                  configuration?.defaultImage && (
+                    <AgilityImage
+                      src={configuration?.defaultImage}
+                      alt=""
+                      width="250"
+                      height="162"
+                      objectFit="contain"
+                    />
+                  )
+                )}
+              </div>
+              <div className="genericCard__titleWithIcon--title">{title}</div>
+            </div>
+          ) : (
+            <div
+              className={`genericCard__image ${
+                configuration?.imageHeight
+                  ? `genericCard__image--${configuration?.imageHeight}`
+                  : ""
+              }`}
+            >
+              {image && image.pixelWidth ? (
+                <AgilityImage
+                  src={image.url}
+                  alt={image.label || ""}
+                  width={image.pixelWidth}
+                  height={image.pixelHeight}
+                  objectFit="cover"
+                  layout="responsive"
+                />
+              ) : (
+                <AgilityImage
+                  src={configuration?.defaultImage}
+                  alt=""
+                  width="250"
+                  height="162"
+                  objectFit="contain"
+                />
+              )}
+            </div>
+          )}
+        </>
+      )}
+      <div className="genericCard__textContent">
+        {date && (
+          <p className="genericCard__textContent--date">{toDate(date)}</p>
+        )}
+        {category && !overrideCategory && (
+          <p className="genericCard__textContent--category">
+            {resolveCategory(category)}
+            {podcast && (
+              <span className="genericCard__textContent--podcast">Podcast</span>
+            )}
+          </p>
+        )}
+        {overrideCategory && (
+          <p className="genericCard__textContent--category">
+            {overrideCategory}
+            {podcast && (
+              <span className="genericCard__textContent--podcast">Podcast</span>
+            )}
+          </p>
+        )}
+        {newsSite && (
+          <p className="genericCard__textContent--newsSite">{newsSite}</p>
+        )}
+        {title && !configuration?.iconStyleImage && (
+          <p
+            className={`genericCard__textContent--title ${
+              configuration?.emphasizedTitle
+                ? "genericCard__textContent--titleEmphasized"
+                : ""
+            }`}
+          >
+            {title}
+          </p>
+        )}
+        {description && (
+          <p
+            className={`genericCard__textContent--description line-clamp ${
+              link ? "" : "pb-3"
+            } ${
+              configuration?.descriptionAlignment
+                ? `align-${configuration.descriptionAlignment}`
+                : "align-left"
+            }`}
+          >
+            {description}
+          </p>
+        )}
+        {link && (
+          <p className="genericCard__textContent--link">
+            <span>Read more</span>
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default RenderGenericCard;
