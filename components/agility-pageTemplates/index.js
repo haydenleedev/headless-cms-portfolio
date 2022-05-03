bcd59395cf0213deafd394be9a2a6995963ba363
Tuuvee <tuukka.tahtinen@hotmail.fi>
2022-05-03 14:07:31 +0300

    feat: added new template for brand pages

--- components/agility-pageTemplates/index.js
+++ components/agility-pageTemplates/index.js
@@ -1,9 +1,9 @@
 import MainTemplate from "./MainTemplate";
-
+import BrandTemplate from "./BrandTemplate";
 // All of the Agility Page Template Components that are in use in this site need to be imported into this index file.
 // Place Page Templates in allTemplates array below, passing in a name and the component.
 
-const allTemplates = [{ name: "MainTemplate", template: MainTemplate }];
+const allTemplates = [{ name: "MainTemplate", template: MainTemplate }, {name: "BrandTemplate", template: BrandTemplate}];
 
 export const getPageTemplate = (templateName) => {
   if (!templateName) return null;
