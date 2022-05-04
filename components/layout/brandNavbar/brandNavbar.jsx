c187ac92e779afe8fc5a066266ed0b78f409f4e3
Tuuvee <tuukka.tahtinen@hotmail.fi>
2022-05-04 11:14:22 +0300

    chore: close menu when navigating to another page

--- components/layout/brandNavbar/brandNavbar.jsx
+++ components/layout/brandNavbar/brandNavbar.jsx
@@ -8,69 +8,73 @@ import GlobalContext from "../../../context";
 import MainNavigation from "../navbar/mainNavigation";
 const BrandNavbar = ({ globalData }) => {
   const { brandNavbar } = globalData.brandNavbar;
-  const copyrightText =globalData.footer.data.fields.copyrightText;
+  const copyrightText = globalData.footer.data.fields.copyrightText;
   const router = useRouter();
   //Affects only mobile
-    const [mainNavigationActive, setMainNavigationActive] = useState(false);
-    const [pageScrolled, setPageScrolled] = useState(false);
-    const [transparentBackground, setTransparentBackground] = useState(false);
-    const [hidden, setHidden] = useState(false);
+  const [mainNavigationActive, setMainNavigationActive] = useState(false);
 
-
-    const handleSetMainNavigationActive = () => {
-      setMainNavigationActive(!mainNavigationActive);
-    };
+  const closeMenu = () => {
+    setMainNavigationActive(false);
+  };
+  const handleSetMainNavigationActive = () => {
+    setMainNavigationActive(!mainNavigationActive);
+  };
   return (
     <section className={`${style.navbar} `}>
-       <nav className={style.nav} role="navigation" aria-label="Main">
-       <button
+      <nav className={style.nav} role="navigation" aria-label="Main">
+        <button
           aria-label="Toggle main navigation menu"
           title="Toggle main navigation menu"
-          onClick={()=>handleSetMainNavigationActive()}
-          className={`${style.navbarToggle
-          }`}
+          onClick={() => handleSetMainNavigationActive()}
+          className={`${style.navbarToggle}`}
         >
           <span></span>
           <span></span>
         </button>
-         <div className={style.logoContainer}>
-        <Link href="/brand/home">
-          <a
-            title="Navigate  to home page"
-            aria-label="Navigate to home page"
-            className={style.brand}
-          >
-            <img
-              className={style.logo}
-              src={logo.src}
-              width="100%"
-              height="100%"
-              alt="Ujet logo"
-            />
-          </a>
-        </Link>
+        <div className={style.logoContainer}>
+          <Link href="/brand/home">
+            <a
+              title="Navigate  to home page"
+              aria-label="Navigate to home page"
+              className={style.brand}
+            >
+              <img
+                className={style.logo}
+                src={logo.src}
+                width="100%"
+                height="100%"
+                alt="Ujet logo"
+              />
+            </a>
+          </Link>
         </div>
         <div
-          className={`${style.mainNavigationContainer } ${
+          className={`${style.mainNavigationContainer} ${
             mainNavigationActive
               ? style.mainNavigationContainerActive
               : style.mainNavigationContainerHidden
           }`}
         >
           <div className={style.mainNavigation}>
-            {brandNavbar.fields.mainNavigation.length >0 && brandNavbar.fields.mainNavigation.map((item, index) =>{
-              return <Link key={`navitem${index}`} href={item.fields.mainLink.fields.link.href} className={style.navLink}>
-                
-               {item.fields.mainLink.fields.link.text}
-              </Link>
-            })}
+            {brandNavbar.fields.mainNavigation.length > 0 &&
+              brandNavbar.fields.mainNavigation.map((item, index) => {
+                return (
+                  <Link
+                    key={`navitem${index}`}
+                    href={item.fields.mainLink.fields.link.href}
+                    className={style.navLink}
+                  >
+                    <a onClick={closeMenu}>
+                      {item.fields.mainLink.fields.link.text}
+                    </a>
+                  </Link>
+                );
+              })}
             <p className={style.copyright}>{copyrightText}</p>
-            </div>
+          </div>
         </div>
         <div className={style.buttonContainer}>
-            <Link href="/">
-              Questions?
-            </Link>
+          <Link href="/">Questions?</Link>
         </div>
       </nav>
     </section>
