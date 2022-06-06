2117fa72df1e4c15d8ac363554399e7ba543e249
haydenleedev <haydenlee22@gmail.com>
2022-06-06 19:11:03 -0400

    updated 6Sense tag and added 6Sense company details API Key

--- shop/components/shopSEO.jsx
+++ shop/components/shopSEO.jsx
@@ -147,6 +147,107 @@ const ShopSEO = ({ seo, children }) => {
       </Script>
 
       <Script id="6sense">
+        {`
+            var processEpsilonData = function(a) {
+              // --- Decode Response ---
+              if (a === '') {
+              // If the response is blank, stop processing
+              return;
+              }
+              var jData = JSON.parse(a);
+              // --- End Decode Response ---
+              
+              // --- Push Company Details to GTM ---
+              window.dataLayer.push({
+              'company_name': jData.company.name,
+              'domain': jData.company.domain,
+              'country': jData.company.country,
+              'address': jData.company.address,
+              'company_state': jData.company.state,
+              'city': jData.company.city,
+              'zip': jData.company.zip,
+              'country_iso_code': jData.company.country_iso_code,
+              'industry': jData.company.industry,
+              'sic': jData.company.sic,
+              'sic_description': jData.company.sic_description,
+              'naics': jData.company.naics,
+              'naics_description': jData.company.naics_description,
+              'employee_range': jData.company.employee_range,
+              'employee_count': jData.company.employee_count,
+              'revenue_range': jData.company.revenue_range,
+              'annual_revenue': jData.company.annual_revenue,
+              'is_blacklisted': jData.company.is_blacklisted,
+              'state_code': jData.company.state_code,
+              'region': jData.company.region,
+              });
+              // --- End Company Details to GTM ---
+              
+              // --- Push Segments to GTM ---
+              window.dataLayer.push({
+              'segment_ids': jData.segments.ids,
+              'segment_names': jData.segments.names,
+              'segment_lists': jData.segments.lists,
+              });
+              if (jData.segments.names) {
+              window.dataLayer.push({
+              'segments': jData.segments.names.join(',')
+              });
+              }
+              // --- End Push Segments to GTM ---
+              
+              // --- Push Product Scores to GTM ---
+              if (jData.scores.length !== 0) {
+              for (var i = 0; i < jData.scores.length; i++) {
+              var product = jData.scores[i].product;
+              var scoreObject = {};
+              scoreObject[product] = jData.scores[i];
+              window.dataLayer.push(scoreObject);
+              }
+              }
+              // --- End Push Product Scores to GTM ---
+               if(jData.scores.length != 0 && jData.scores[0]) {
+              var score = jData.scores[0];
+              window.dataLayer.push({
+              'buying_stage': score.buying_stage,
+              'profile_fit': score.profile_fit,
+              })
+              }
+              // --- Send Confidence Score to GTM ---
+              window.dataLayer.push({
+              'confidence': jData.confidence
+              });
+              // --- End Send Confidence Score to GTM ---
+              
+              // --- Trigger Company Details Loaded in GTM ---
+              window.dataLayer.push({
+              'event': '6si_company_details_loaded'
+              });
+              // --- End Trigger Company Details Loaded in GTM ---
+              };
+              
+              window._6si = window._6si || [];
+              window.dataLayer = window.dataLayer || [];
+              window._6si.push(['enableEventTracking', true]);
+              window._6si.push(['setToken', '${process.env.NEXT_PUBLIC_SIXSENSE_TOKEN}']); // REPLACE ME
+              window._6si.push(['setEpsilonKey', '${process.env.NEXT_PUBLIC_SIXSENSE_COMPANY_DETAILS_API_KEY}']); // REPLACE ME
+              window._6si.push(["setEndpoint", "b.6sc.co"]);
+              var epsilonName = 'enableCompanyDetails';
+              var enabled = true; // set to true to enable API
+              var callback = processEpsilonData; // optional callback
+              var version = 3; // 3 for v3
+              window._6si.push([epsilonName, enabled, callback, version]);
+              (function() {
+              var gd = document.createElement('script');
+              gd.type = 'text/javascript';
+              gd.async = true;
+              gd.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'j.6sc.co/6si.min.js';
+              var s = document.getElementsByTagName('script')[0];
+              s.parentNode.insertBefore(gd, s);
+              })();
+          `}
+      </Script>
+
+      {/* <Script id="6sense">
         {`window._6si = window._6si || [];
          window._6si.push(['enableEventTracking', true]);
          window._6si.push(['setToken', '${process.env.NEXT_PUBLIC_SIXSENSE_TOKEN}']);
@@ -160,7 +261,7 @@ const ShopSEO = ({ seo, children }) => {
            var s = document.getElementsByTagName('script')[0];
            s.parentNode.insertBefore(gd, s);
          })();`}
-      </Script>
+      </Script> */}
 
       {/* Qualified Script */}
       {/*  <Script id="qualified">
