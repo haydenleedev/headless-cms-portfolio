14bf88078dec75a6e642c7b08e4e43e41673ddb2
haydenleedev <haydenlee22@gmail.com>
2022-06-06 17:14:33 -0400

    removed Alexa Script

--- components/SEO.jsx
+++ components/SEO.jsx
@@ -263,12 +263,12 @@ const SEO = ({ title, description, keywords, metaHTML, url }) => {
             src={`${googleOptimize}${process.env.NEXT_PUBLIC_GOOGLE_OPTIMIZE_ID}`}
             strategy="lazyOnload"
           /> */}
-          <Script id="ax" strategy="afterInteractive">
+          {/* <Script id="ax" strategy="afterInteractive">
             {`
           _atrk_opts = { atrk_acct:"xw4cw1Y1Mn20Io", domain:"ujet.cx",dynamic: true};
           (function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://certify-js.alexametrics.com/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
         `}
-          </Script>
+          </Script> */}
           <Script id="g2Crowd" strategy="afterInteractive">
             {`
           (function (c, p, d, u, id, i) {
