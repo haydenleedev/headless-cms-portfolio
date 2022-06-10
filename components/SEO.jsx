9c317c4015898ad5ed92c7faca9990d6f6e937ff
Kim B <kim@ikius.com>
2022-06-10 11:37:03 +0300

    Re-enable OneTrust script for non-brand pages

--- components/SEO.jsx
+++ components/SEO.jsx
@@ -220,17 +220,17 @@ const SEO = ({
               />
             </>
           )}
-          {/* <Script
-        id="onetrust"
-        src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"
-        charSet="UTF-8"
-        strategy="beforeInteractive"
-        data-domain-script={`${process.env.NEXT_PUBLIC_ONETRUST_DATA_DOMAIN_SCRIPT}`}
-      />
+          <Script
+            id="onetrust"
+            src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"
+            charSet="UTF-8"
+            strategy="beforeInteractive"
+            data-domain-script={`${process.env.NEXT_PUBLIC_ONETRUST_DATA_DOMAIN_SCRIPT}`}
+          />
 
-      <Script id="optanon-wrapper">
-        {`function OptanonWrapper() { }`}
-      </Script> */}
+          <Script id="optanon-wrapper">
+            {`function OptanonWrapper() { }`}
+          </Script>
         </>
       )}
     </>
