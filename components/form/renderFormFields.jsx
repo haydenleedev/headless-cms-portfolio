448e1662da12e1ff838757f8d456ea741c3c8f67
Max I <max@ikius.com>
2022-07-01 15:02:21 +0300

    feat: expand renderFormFields logic and add blacklist for  pardot form handler fields

--- components/form/renderFormFields.jsx
+++ components/form/renderFormFields.jsx
@@ -1,82 +1,119 @@
-// import { useEffect, useRef, useState } from "react";
-
-const getGormField = (field) => {
-  console.log("Getting form field for: ", field.dataFormat);
-  switch (field.dataFormat) {
-    case "email":
-      return (
-        <div>
-          <label htmlFor={field.id}>
-            <span className="">*</span> {field.name}
-          </label>
-          <input
-            name={field.name}
-            id={field.id}
-            autoComplete="email"
-            maxLength="50"
-            required={field.isRequired}
-          />
-        </div>
-      );
-    case "phone":
-      return (
-        <div>
-          <label htmlFor={field.id}>
-            <span className="">*</span> {field.name}
-          </label>
-          <input name="phone" id="phone" title="Phone Number"></input>
-        </div>
-      );
-    case "text":
-      return (
-        <div>
-          <label htmlFor={field.id}>
-            <span className="">*</span> {field.name}
-          </label>
-          <input
-            name={field.name}
-            id={field.id}
-            maxLength="50"
-            required={field.isRequired}
-          />
-        </div>
-      );
-    // case "select":
-    //   <div>
-    //     <label htmlFor={field.id}>
-    //       <span className="">*</span> {field.name}
-    //     </label>
-    //     <select
-    //       name={field.name}
-    //       id={field.id}
-    //       title="State"
-    //       aria-labelledby="LblState InstructState"
-    //       aria-invalid="true"
-    //     >
-    //       {field.options?.map((option) => (
-    //         <option value={option.value}>{option.label}</option>
-    //       ))}
-    //     </select>
-    //   </div>;
-    default:
-      return (
-        <div>
-          <label htmlFor={field.id}>
-            <span className="">*</span> {field.name}
-          </label>
-          <input
-            name={field.name}
-            id={field.id}
-            maxLength="50"
-            required={field.isRequired}
-          />
-        </div>
-      );
-  }
-};
+import { useEffect, useRef, useState } from "react";
+import style from "./renderFormFields.module.scss";
 
 const RenderFormFields = ({ fields }) => {
-  return fields.map((field) => getGormField(field));
+  const [errors, setErrors] = useState([]);
+
+  const isHiddenField = (field) => {
+    // Blacklist hidden fields from Pardot form handler fields
+    const hiddenFields = [/ga_/, /utm_/, /current lead/, /hidden/, /hide/];
+
+    // Check whether form field is blacklisted
+    if (
+      hiddenFields.some((re) => re.test(String(field.name).toLocaleLowerCase()))
+    )
+      return true;
+    return false;
+  };
+
+  const FormFieldWrapper = ({ field, children }) => (
+    <div
+      className={isHiddenField(field) ? style.formFieldHidden : style.formField}
+    >
+      <FormFieldLabel field={field}></FormFieldLabel>
+      {children}
+      <FormFieldError field={field}></FormFieldError>
+    </div>
+  );
+
+  const FormFieldLabel = ({ field }) => (
+    <label htmlFor={field.id}>
+      <span className="">*</span> {field.name}
+    </label>
+  );
+
+  // TODO: create error handling by pushing error field id to array.
+  const FormFieldError = ({ field }) => {
+    if (errors.includes(fields.id)) {
+      return <span className={style.error}>{field.errorMessage}</span>;
+    } else {
+      return null;
+    }
+  };
+
+  const getFormField = (field) => {
+    console.log("Getting form field for: ", field.dataFormat);
+    switch (field.dataFormat) {
+      case "email":
+        return (
+          <FormFieldWrapper field={field}>
+            <input
+              hidden={isHiddenField(field)}
+              name={field.name}
+              id={field.id}
+              autoComplete="email"
+              maxLength="50"
+              required={field.isRequired}
+            />
+          </FormFieldWrapper>
+        );
+      // TODO: Phoen is also returned as number type, so have to detect if field.name contains "Phone",
+      // And render phone type and not number....
+      case "phone":
+        return (
+          <FormFieldWrapper field={field}>
+            <input
+              name={field.name}
+              id={field.id}
+              type="phone"
+              title={field.name}
+              hidden={isHiddenField(field)}
+            ></input>
+          </FormFieldWrapper>
+        );
+      case "number":
+        return (
+          <FormFieldWrapper field={field}>
+            <input
+              name={field.name}
+              id={field.id}
+              type="number"
+              title={field.name}
+              hidden={isHiddenField(field)}
+            ></input>
+          </FormFieldWrapper>
+        );
+      case "text":
+        return (
+          <FormFieldWrapper field={field}>
+            <input
+              name={field.name}
+              id={field.id}
+              maxLength="50"
+              hidden={isHiddenField(field)}
+              required={field.isRequired}
+            />
+          </FormFieldWrapper>
+        );
+      // TODO: create select field logic since pardot only return "Text" fields
+      // So we have to detect fake select fields and render them accordingly
+      // Ask Max for more
+      // case "select":
+      default:
+        return (
+          <FormFieldWrapper field={field}>
+            <input
+              name={field.name}
+              id={field.id}
+              maxLength="50"
+              required={field.isRequired}
+            />
+          </FormFieldWrapper>
+        );
+    }
+  };
+
+  return fields.map((field) => getFormField(field));
 };
 
 export default RenderFormFields;
