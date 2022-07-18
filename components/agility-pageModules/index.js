f192f11f6f3a85f5709de2405fd54a0c135e4eb0
haydenleedev <haydenlee22@gmail.com>
2022-07-18 18:25:32 -0400

    removed deal registration module

--- components/agility-pageModules/index.js
+++ components/agility-pageModules/index.js
@@ -56,7 +56,6 @@ import GlossaryPageContent from "./glossaryPageContent/glossaryPageContent";
 import ShopErrorPageContent from "./shopErrorPageContent/shopErrorPageContent";
 import JobOpeningList from "./jobOpeningList/jobOpeningList";
 import VideoPopup from "./videoPopup/videoPopup";
-import DealRegistration from "./dealRegistration/dealRegistration";
 import BrandFirstFold from "../../brand/components/brandFirstFold/brandFirstFold";
 import BrandTextWithMedia from "../../brand/components/brandTextWithMedia/brandTextWithMedia";
 import BrandTwoTextColumns from "../../brand/components/brandTwoTextColumns/brandTwoTextColumns";
@@ -120,7 +119,6 @@ const allModules = [
   { name: "ShopErrorPageContent", module: ShopErrorPageContent },
   { name: "JobOpeningList", module: JobOpeningList },
   { name: "VideoPopup", module: VideoPopup },
-  { name: "DealRegistration", module: DealRegistration },
   { name: "BrandFirstFold", module: BrandFirstFold },
   { name: "BrandTextGridWithMedia", module: BrandTextGridWithMedia },
   { name: "BrandTextWithMedia", module: BrandTextWithMedia },
