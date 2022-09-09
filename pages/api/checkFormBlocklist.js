98875bdfde67756371e94ffe378ac4627bfb4995
Kim B <kim@ikius.com>
2022-09-09 16:30:53 +0300

    Update Google Sheets URLs in serverless functions

--- pages/api/checkFormBlocklist.js
+++ pages/api/checkFormBlocklist.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     const postData = new URLSearchParams();
     postData.append("ip", parsedBody.ip);
     const response = await fetch(
-      "https://script.google.com/macros/s/AKfycbyDgAwr8EsqrCq4vLXpwbgUt8sqLkeUzTBK89Xa04SL-Mgbq5o/exec",
+      "https://script.google.com/macros/s/AKfycby13GURhWG9DmUJk1qLy9XeITpQP_grLWJhshoLu13fgt5CGbs/exec",
       {
         method: "POST",
         body: postData,
