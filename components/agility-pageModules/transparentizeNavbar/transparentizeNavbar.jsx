ef5c68b78f7851ca4d80321e7d4f2cc0cbcc9bd8
Aatu Väisänen <aatu@ikius.com>
2022-09-13 14:56:51 +0300

    chore: make vertical offset fix optional for TransparentizeNavbar

--- components/agility-pageModules/transparentizeNavbar/transparentizeNavbar.jsx
+++ components/agility-pageModules/transparentizeNavbar/transparentizeNavbar.jsx
@@ -1,16 +1,22 @@
 // this is an utility module which toggles the navbar's transparency on when placed on top of a page on agility.
 
 import { useEffect, useRef } from "react";
+import { boolean } from "../../../utils/validation";
 
-const TransparentizeNavbar = () => {
+const TransparentizeNavbar = ({ module }) => {
   const moduleRef = useRef(null);
+  const { fields } = module;
+  const verticalOffsetFix = boolean(fields?.verticalOffsetFix);
 
   useEffect(() => {
     // get the next sibling module to make it have extra padding and top position to compensate for the transparentized navbar
     // but ONLY if this module is placed on top of the page module list
     if (moduleRef.current && !moduleRef.current.previousSibling) {
       const nextSiblingModule = moduleRef.current.nextSibling;
+      const followingSiblingModule = nextSiblingModule.nextSibling; // sibling after nextSiblingModule
       nextSiblingModule.classList.add("transparentized-navbar-transform");
+      if (verticalOffsetFix)
+        followingSiblingModule.classList.add("mt--navbar-height");
     }
   }, [moduleRef.current]);
   return <section data-transparent-navbar="true" ref={moduleRef}></section>;
