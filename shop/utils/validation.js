db7187ef995efbb31f4252888f1fdc043d066741
haydenleedev <haydenlee22@gmail.com>
2022-09-27 11:19:09 -0400

    added free email domain validator

--- shop/utils/validation.js
+++ shop/utils/validation.js
@@ -11,3 +11,25 @@ export function isWebsite(website) {
 export function isPhoneNumber(phoneNumber) {
   return phoneNumber.match(usPhoneRegex);
 }
+
+const freeEmailDomains = [
+  /@gmail.com/,
+  /@yahoo.com/,
+  /@msn.com/,
+  /@comcast.net/,
+  /@hotmail.com/,
+  /@icloud.com/,
+];
+let freeEmailRegex;
+
+export function isFreeEmail(email) {
+  if (
+    freeEmailDomains.some((domain) =>
+      domain.test(String(email).toLocaleLowerCase())
+    )
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
