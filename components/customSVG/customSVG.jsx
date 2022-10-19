4c019f4356cff126d7b2212950120e23c37ab323
Aatu Väisänen <aatu@ikius.com>
2022-10-19 10:47:32 +0300

    feat: add animate option for disabling any svg animations from First Fold

--- components/customSVG/customSVG.jsx
+++ components/customSVG/customSVG.jsx
@@ -1,7 +1,9 @@
-const CustomSVG = ({ svgClasses, svgInput }) => {
+const CustomSVG = ({ svgClasses, svgInput, animateSVG }) => {
+  const disableAnimation =
+    animateSVG === "static" ? "first-fold-svg-animation-unset" : "";
   return (
     <div
-      className={svgClasses}
+      className={`${svgClasses} ${disableAnimation}`}
       dangerouslySetInnerHTML={{ __html: svgInput }}
     ></div>
   );
