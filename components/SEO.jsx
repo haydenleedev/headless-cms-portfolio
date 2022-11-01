23a7a14d98d2aef2731189a4f8267cf55b59fc4d
Aatu Väisänen <aatu@ikius.com>
2022-11-01 16:23:34 +0200

    Revert "chore: performance test"
    This reverts commit f201ddd4a0b8145d4e9d01759feb58a0eb9c5d96.

--- components/SEO.jsx
+++ components/SEO.jsx
@@ -124,7 +124,7 @@ const SEO = ({
 
         {/* TODO: add Canonical url */}
       </Head>
-      {/* {pageTemplateName !== "BrandTemplate" && (
+      {pageTemplateName !== "BrandTemplate" && (
         <>
           {timerExpired && (
             <>
@@ -248,8 +248,10 @@ const SEO = ({
           }("1136", document.location.href, document));
         `}
               </Script>
+              {/* Load Qualified script after user starts scrolling */}
               {scrolled && (
                 <>
+                  {/* Qualified Script */}
                   <Script id="qualified" strategy="lazyOnload">
                     {`(function(w,q){w['QualifiedObject']=q;w[q]=w[q]||function(){
           (w[q].q=w[q].q||[]).push(arguments)};})(window,'qualified')`}
@@ -274,7 +276,7 @@ const SEO = ({
             </>
           )}
         </>
-      )} */}
+      )}
     </>
   );
 };
