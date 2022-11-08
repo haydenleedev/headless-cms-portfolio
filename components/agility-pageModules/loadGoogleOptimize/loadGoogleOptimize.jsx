26d3c547566c9114827ccb82b57fa762c682e3da
Aatu Väisänen <aatu@ikius.com>
2022-11-08 09:35:53 +0200

    chore: performance test

--- components/agility-pageModules/loadGoogleOptimize/loadGoogleOptimize.jsx
+++ components/agility-pageModules/loadGoogleOptimize/loadGoogleOptimize.jsx
@@ -2,21 +2,22 @@ import Script from "next/script";
 import { useEffect, useState } from "react";
 
 const LoadGoogleOptimize = () => {
-  const [timerExpired, setTimerExpired] = useState(false);
+  /*   const [timerExpired, setTimerExpired] = useState(false);
   useEffect(() => {
     // Delay script loading with setTimeout
     setTimeout(() => {
       setTimerExpired(true);
     }, 0);
   }, []);
-  const googleOptimize = "https://www.googleoptimize.com/optimize.js?id=";
-  return timerExpired ? (
+  const googleOptimize = "https://www.googleoptimize.com/optimize.js?id="; */
+  return null;
+  /* timerExpired ? (
     <Script
       id="google-optimize"
       src={`${googleOptimize}${process.env.NEXT_PUBLIC_GOOGLE_OPTIMIZE_ID}`}
       strategy="lazyOnload"
     />
-  ) : null;
+  ) : */ null;
 };
 
 export default LoadGoogleOptimize;
