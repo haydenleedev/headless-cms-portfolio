eb288ac01e8f8d343d99074fd7676362f219829b
Aatu Väisänen <aatu@ikius.com>
2022-12-05 10:17:58 +0200

    Revert "chore: customerstories, capitalise firs tletter of title when using first fold data"
    This reverts commit f345e4ef83dd167990341a13c93e1a20c87180f6.

--- components/agility-pageModules/latestCustomerStories/latestCustomerStoriesContent.jsx
+++ components/agility-pageModules/latestCustomerStories/latestCustomerStoriesContent.jsx
@@ -1,7 +1,7 @@
 import dynamic from "next/dynamic";
 import { Fragment, useState } from "react";
 import style from "./latestCustomerStories.module.scss";
-import { cleanText, capitalizeFirstLetter } from "../../../utils/convert";
+import { cleanText } from "../../../utils/convert";
 const Heading = dynamic(() => import("../heading"), { ssr: false });
 const GenericCard = dynamic(() => import("../../genericCard/genericCard"), {
   ssr: false,
@@ -32,7 +32,7 @@ const LatestCustomerStoriesContent = ({
                 <GenericCard
                   link={{ href: `${rootPath}/${story.name}` }}
                   image={story.fields.image || story.fields.media}
-                  title={story.fields.title || capitalizeFirstLetter(story.name)}
+                  title={story.fields.title || story.name}
                   ariaTitle={`${story.fields.title || story.name} customer story`}
                   description={story.fields.description || cleanText(story.fields.text)}
                   configuration={{
