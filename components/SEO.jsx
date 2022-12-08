6dacf88155eb068ccfb93b83a5eaa895ada8f821
Aatu Väisänen <aatu@ikius.com>
2022-12-08 12:23:37 +0200

    chore: load gtm in a web worker

--- components/SEO.jsx
+++ components/SEO.jsx
@@ -211,7 +211,7 @@ const SEO = ({
           <>
             {(timerExpired || userInteracted) && (
               <>
-                <Script id="google-tag-manager">
+                <Script id="google-tag-manager" strategy="worker">
                   {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
