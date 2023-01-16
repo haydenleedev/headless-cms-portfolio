560631a64a1c2e121576249fd760c2345dbc4ffa
Aatu Väisänen <aatu@ikius.com>
2023-01-16 16:27:12 +0200

    chore: move onetrust script back to SEO.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,30 +1,9 @@
 import "../styles/main.scss";
 import GlobalContextWrapper from "../context/globalContextWrapper";
-import Script from "next/script";
-import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const [canLoadOneTrust, setCanLoadOneTrust] = useState(false);
-  useEffect(() => {
-    setTimeout(() => {
-      setCanLoadOneTrust(true);
-    }, 0);
-  }, []);
-
   return (
     <GlobalContextWrapper>
-      {pageProps.pageTemplateName !== "BrandTemplate" && canLoadOneTrust && (
-        <>
-          <Script
-            id="onetrust"
-            src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"
-            charSet="UTF-8"
-            data-domain-script={`${process.env.NEXT_PUBLIC_ONETRUST_DATA_DOMAIN_SCRIPT}`}
-          />
-
-          <Script id="optanon-wrapper">{`function OptanonWrapper() { }`}</Script>
-        </>
-      )}
       <Component {...pageProps} />
     </GlobalContextWrapper>
   );
