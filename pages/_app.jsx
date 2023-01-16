decb7ce435aab4dbcd1352bca0a294bab8c13565
Aatu Väisänen <aatu@ikius.com>
2023-01-16 15:50:34 +0200

    chore: load onetrust in _document

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,25 +1,9 @@
 import "../styles/main.scss";
 import GlobalContextWrapper from "../context/globalContextWrapper";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  // OneTrust should be loaded in _app.jsx because it has the beforeInteractive loading strategy.
-  // More info: https://nextjs.org/docs/api-reference/next/script#beforeinteractive
   return (
     <GlobalContextWrapper>
-      {pageProps.pageTemplateName !== "BrandTemplate" && (
-        <>
-          <Script
-            id="onetrust"
-            src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"
-            charSet="UTF-8"
-            strategy="beforeInteractive"
-            data-domain-script={`${process.env.NEXT_PUBLIC_ONETRUST_DATA_DOMAIN_SCRIPT}`}
-          />
-
-          <Script id="optanon-wrapper">{`function OptanonWrapper() { }`}</Script>
-        </>
-      )}
       <Component {...pageProps} />
     </GlobalContextWrapper>
   );
---
+++ pages/_document.jsx
@@ -0,0 +1,26 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  // OneTrust should be loaded in _document.jsx because it has the beforeInteractive loading strategy.
+  // More info: https://nextjs.org/docs/api-reference/next/script#beforeinteractive
+  // Not using next/script here because of https://github.com/vercel/next.js/issues/36997
+  // TODO: replace native script tag with next/script (beforeInteractive) when next.js version is upgraded.
+  return (
+    <Html>
+      <Head>
+        <script
+          type="text/javascript"
+          id="onetrust"
+          src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"
+          defer
+          data-domain-script={`${process.env.NEXT_PUBLIC_ONETRUST_DATA_DOMAIN_SCRIPT}`}
+        ></script>
+        <script id="optanon-wrapper">{`function OptanonWrapper() { }`}</script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
