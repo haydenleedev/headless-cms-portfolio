f87315d1ab87cf4167cd048db2db2618a813e778
Aatu Väisänen <aatu@ikius.com>
2023-01-16 16:06:45 +0200

    chore: adjust onetrust loading

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,19 +1,25 @@
 import "../styles/main.scss";
 import GlobalContextWrapper from "../context/globalContextWrapper";
 import Script from "next/script";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const [canLoadOneTrust, setCanLoadOneTrust] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setCanLoadOneTrust(true);
+    }, 0);
+  }, []);
   // OneTrust should be loaded in _app.jsx because it has the beforeInteractive loading strategy.
   // More info: https://nextjs.org/docs/api-reference/next/script#beforeinteractive
   return (
     <GlobalContextWrapper>
-      {pageProps.pageTemplateName !== "BrandTemplate" && (
+      {pageProps.pageTemplateName !== "BrandTemplate" && canLoadOneTrust && (
         <>
           <Script
             id="onetrust"
             src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"
             charSet="UTF-8"
-            strategy="beforeInteractive"
             data-domain-script={`${process.env.NEXT_PUBLIC_ONETRUST_DATA_DOMAIN_SCRIPT}`}
           />
 
