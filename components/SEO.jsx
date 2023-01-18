127eb9d521545326fc8927d59e15a348c1d3c52d
Aatu Väisänen <aatu@ikius.com>
2023-01-18 15:15:04 +0200

    script loading adjustment

--- components/SEO.jsx
+++ components/SEO.jsx
@@ -68,7 +68,11 @@ const SEO = ({
 
     setTimeout(() => {
       setConsentTimerExpired(true);
-    }, 2500);
+    }, 0);
+    // Load other scripts anyway after 5 seconds, if user interaction was not detected.
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, 5000);
 
     // get gclid values
     function getParam(p) {
@@ -249,24 +253,20 @@ const SEO = ({
                 id="onetrust"
                 src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"
                 charSet="UTF-8"
+                strategy="afterInteractive"
                 data-domain-script={`${process.env.NEXT_PUBLIC_ONETRUST_DATA_DOMAIN_SCRIPT}`}
-                onLoad={() => {
-                  setTimeout(() => {
-                    setTimerExpired(true);
-                  }, 1000);
-                }}
               />
 
               <Script id="optanon-wrapper">{`function OptanonWrapper() { }`}</Script>
             </>
           )}
           <>
-            {timerExpired && (
+            {(timerExpired || userInteracted) && (
               <>
                 <Script id="google-tag-manager">
                   {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');`}
                 </Script>
