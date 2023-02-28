57251ebf322478ca60218527b702dfef6f899f3b
Aatu Väisänen <aatu@ikius.com>
2023-02-28 16:39:48 +0200

    feat: update resourceList to use individual resource item links instead of list

--- components/agility-pageModules/resourceList/resourceListContent.jsx
+++ components/agility-pageModules/resourceList/resourceListContent.jsx
@@ -25,25 +25,63 @@ const ResourceListContent = ({ fields }) => {
         </div>
       )}
       <div className={style.resources}>
-        {fields.highlightedResources.map((resource, index) => (
-          <div className={style.resource} key={resource.contentID}>
+        {fields.resource1 && (
+          <div className={style.resource}>
             <GenericCard
               link={resolveLink(
-                resource.properties.referenceName,
-                resource.fields
+                fields.resource1.properties.referenceName,
+                fields.resource1.fields
               )}
-              category={resource.properties.referenceName}
-              overrideCategory={resource.fields.cardCategoryTitle}
-              title={resource.fields.title}
-              ariaTitle={resource.fields.title}
+              category={fields.resource1.properties.referenceName}
+              overrideCategory={fields.resource1.fields.cardCategoryTitle}
+              title={fields.resource1.fields.title}
+              ariaTitle={fields.resource1.fields.title}
               image={
-                placeholderImages?.length > index
-                  ? placeholderImages[index]
-                  : resource.fields.image
+                placeholderImages?.length > 0
+                  ? placeholderImages[0]
+                  : fields.resource1.fields.image
               }
             />
           </div>
-        ))}
+        )}
+        {fields.resource2 && (
+          <div className={style.resource}>
+            <GenericCard
+              link={resolveLink(
+                fields.resource2.properties.referenceName,
+                fields.resource2.fields
+              )}
+              category={fields.resource2.properties.referenceName}
+              overrideCategory={fields.resource2.fields.cardCategoryTitle}
+              title={fields.resource2.fields.title}
+              ariaTitle={fields.resource2.fields.title}
+              image={
+                placeholderImages?.length > 1
+                  ? placeholderImages[1]
+                  : fields.resource2.fields.image
+              }
+            />
+          </div>
+        )}
+        {fields.resource3 && (
+          <div className={style.resource}>
+            <GenericCard
+              link={resolveLink(
+                fields.resource3.properties.referenceName,
+                fields.resource3.fields
+              )}
+              category={fields.resource3.properties.referenceName}
+              overrideCategory={fields.resource3.fields.cardCategoryTitle}
+              title={fields.resource3.fields.title}
+              ariaTitle={fields.resource3.fields.title}
+              image={
+                placeholderImages?.length > 2
+                  ? placeholderImages[2]
+                  : fields.resource3.fields.image
+              }
+            />
+          </div>
+        )}
       </div>
     </nav>
   );
