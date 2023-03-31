21937aabd1b9bc463ad2ee9e1fc41656247a8148
Aatu Väisänen <aatu@ikius.com>
2023-03-31 16:04:54 +0300

    feat: allow using row data or column data in flexColumnTableWithHeading

--- components/agility-pageModules/flexColumnTableWithHeading/flexColumnTableWithHeading.jsx
+++ components/agility-pageModules/flexColumnTableWithHeading/flexColumnTableWithHeading.jsx
@@ -1,9 +1,14 @@
 import dynamic from "next/dynamic";
 import { cn } from "../../../utils/generic";
 import style from "./flexColumnTableWithHeading.module.scss";
+import { boolean } from "../../../utils/validation";
+import { useEffect, useState } from "react";
+import variables from "../../../styles/settings/variables.module.scss";
+import { remToPixels } from "../../../utils/convert";
 const Heading = dynamic(() =>
   import("../../../components/agility-pageModules/heading")
 );
+
 const FlexColumnTableWithHeading = ({
   module: {
     fields: {
@@ -14,10 +19,13 @@ const FlexColumnTableWithHeading = ({
       backgroundColor,
       tableRowsColorScheme,
       tableColumns,
+      tableRows,
       heading,
     },
   },
 }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
   // Margins & Paddings
   const mtValue = marginTop ? marginTop : "";
   const mbValue = marginBottom ? marginBottom : "";
@@ -25,6 +33,26 @@ const FlexColumnTableWithHeading = ({
   const pbValue = paddingBottom ? paddingBottom : "";
 
   const sectionHeading = heading ? JSON.parse(heading) : null;
+
+  const headingRow = tableRows?.find((row) => boolean(row.fields.isHeadingRow));
+  const rowTableColumnCount = headingRow.fields.cells.length;
+
+  const longestColumn = tableColumns?.reduce((prev, current) =>
+    prev.fields.cells.length > current.fields.cells.length ? prev : current
+  ).fields.cells.length;
+
+  const checkMobile = () => {
+    if (window.innerWidth < remToPixels(variables.largeDesktop))
+      setIsMobile(true);
+    else setIsMobile(false);
+  };
+  useEffect(() => {
+    checkMobile();
+    window?.addEventListener("resize", checkMobile);
+    return () => {
+      window?.removeEventListener("resize", checkMobile);
+    };
+  }, []);
 
   return (
     <section
@@ -43,26 +71,97 @@ const FlexColumnTableWithHeading = ({
           <div className={style.headingWrapper}>
             <Heading {...sectionHeading} />
           </div>
-          {tableColumns?.length > 0 && (
-            <div
-              className={style.table}
-              style={{
-                gridTemplateColumns: `repeat(${tableColumns.length}, minmax(0, 1fr))`,
-              }}
-            >
-              {tableColumns.map((column, i) => (
-                <table className={style.tableColumn} key={i}>
-                  <tr className={style.columnTitle}>
-                    <th>{column.fields.columnTitle}</th>
-                  </tr>
-                  {column.fields.cells.map((cell, j) => (
-                    <tr key={j} className={style.columnCell}>
-                      <td>{cell.fields.value}</td>
+          {tableRows?.length > 0 ? (
+            <table className={style.rowTable}>
+              {tableRows
+                .sort((prev, current) => {
+                  if (
+                    boolean(prev.isHeadingRow) &&
+                    !boolean(current.isHeadingRow)
+                  )
+                    return 1;
+                  else if (
+                    !boolean(prev.isHeadingRow) &&
+                    boolean(current.isHeadingRow)
+                  )
+                    return -1;
+                  return 0;
+                })
+                .map((row, i) =>
+                  row.fields.isHeadingRow ? (
+                    <tr
+                      key={i}
+                      className={`${style.columnTitle} ${
+                        isMobile ? "is-hidden" : ""
+                      }`}
+                      style={{
+                        gridTemplateColumns: `repeat(${rowTableColumnCount}, minmax(0, 1fr))`,
+                      }}
+                    >
+                      {row.fields.cells
+                        .slice(0, rowTableColumnCount)
+                        .map((cell, j) => (
+                          <th key={j}>{cell.fields.value}</th>
+                        ))}
                     </tr>
+                  ) : (
+                    <tr
+                      key={i}
+                      className={style.columnCell}
+                      style={{
+                        gridTemplateColumns: `repeat(${rowTableColumnCount}, minmax(0, 1fr))`,
+                      }}
+                    >
+                      {isMobile
+                        ? row.fields.cells
+                            .slice(0, rowTableColumnCount)
+                            .map((cell, j) => (
+                              <td key={j}>
+                                <b>
+                                  {headingRow.fields.cells[j].fields.value}:
+                                </b>{" "}
+                                {cell.fields.value}
+                              </td>
+                            ))
+                        : row.fields.cells
+                            .slice(0, rowTableColumnCount)
+                            .map((cell, j) => (
+                              <td key={j}>{cell.fields.value}</td>
+                            ))}
+                    </tr>
+                  )
+                )}
+            </table>
+          ) : (
+            <>
+              {tableColumns?.length > 0 && (
+                <div
+                  className={style.columnTable}
+                  style={{
+                    gridTemplateColumns: `repeat(${tableColumns.length}, minmax(0, 1fr))`,
+                  }}
+                >
+                  {tableColumns.map((column, i) => (
+                    <table className={style.tableColumn} key={i}>
+                      <thead>
+                        <tr className={style.columnTitle}>
+                          <th>{column.fields.columnTitle}</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {Array(longestColumn)
+                          .fill(0)
+                          .map((key, j) => (
+                            <tr key={j} className={style.columnCell}>
+                              <td>{column.fields.cells[j]?.fields?.value}</td>
+                            </tr>
+                          ))}
+                      </tbody>
+                    </table>
                   ))}
-                </table>
-              ))}
-            </div>
+                </div>
+              )}
+            </>
           )}
         </div>
       </div>
